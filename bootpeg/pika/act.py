7cba97e540ca99d74418840f47110b158c14d54b
Max Fischer <maxfischer2781@gmail.com>
2021-03-18 17:22:08 +0100

    Add Pika Parser (#1)
    * basic implementation of Pika parser
    
    * moved pika parser to submodule
    
    * working parser for EBNF/PEG
    
    * Pika boot PEG
    
    * added Pika to README

---
+++ bootpeg/pika/act.py
@@ -0,0 +1,163 @@
+"""
+Pika bottom-up Peg parser extension to transform parsed source
+"""
+from typing import TypeVar, Any, Dict, Tuple
+import re
+
+from .peg import Clause, D, Match, MemoTable, MemoKey, Literal, nested_str
+from ..utility import mono
+
+
+#: Parser result: The output type for parsing, such as (str, ...)
+R = TypeVar("R", contravariant=True)
+
+
+class Debug(Clause[D]):
+    def __init__(self, sub_clause, name: str = None):
+        self.sub_clauses = (sub_clause,)
+        self.name = name or str(sub_clause)
+
+    @property
+    def maybe_zero(self):
+        return self.sub_clauses[0].maybe_zero
+
+    def match(self, source: D, at: int, memo: MemoTable):
+        try:
+            parent_match = memo[MemoKey(at, self.sub_clauses[0])]
+        except KeyError:
+            return None
+        else:
+            print('match', self.name, 'at', at, ':', parent_match.length)
+            print("'", mono(source[at:at + parent_match.length]), "'", sep="")
+            return parent_match
+
+    def __eq__(self, other):
+        return isinstance(other, Literal) and self.sub_clauses == other.sub_clauses
+
+    def __hash__(self):
+        return hash(self.sub_clauses)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.sub_clauses[0]!r})"
+
+    def __str__(self):
+        return f":{self.name}:"
+
+
+class Capture(Clause[D]):
+    """Capture the result of matching a clause by name for an :py:class:`~.Action`"""
+    __slots__ = ('name', 'sub_clauses')
+
+    def __init__(self, name, sub_clause: Clause[D]):
+        self.name = name
+        self.sub_clauses = sub_clause,
+
+    @property
+    def maybe_zero(self):
+        return self.sub_clauses[0].maybe_zero
+
+    def match(self, source: D, at: int, memo: MemoTable):
+        parent_match = memo[MemoKey(at, self.sub_clauses[0])]
+        return Match(parent_match.length, (parent_match,), at, self)
+
+    def __eq__(self, other):
+        return isinstance(other, Capture) and self.name == other.name and self.sub_clauses == other.sub_clauses
+
+    def __hash__(self):
+        return hash((self.name, self.sub_clauses))
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.sub_clauses[0]!r})"
+
+    def __str__(self):
+        return f"{self.name}={nested_str(self.sub_clauses[0])}"
+
+
+class Rule(Clause[D]):
+    __slots__ = ('sub_clauses', 'action')
+
+    def __init__(self, sub_clause: Clause[D], action):
+        self.sub_clauses = sub_clause,
+        self.action = action
+
+    @property
+    def maybe_zero(self):
+        return self.sub_clauses[0].maybe_zero
+
+    def match(self, source: D, at: int, memo: MemoTable):
+        parent_match = memo[MemoKey(at, self.sub_clauses[0])]
+        return Match(parent_match.length, (parent_match,), at, self)
+
+    def __eq__(self, other):
+        return isinstance(other, Rule) and self.action == other.action and self.sub_clauses == other.sub_clauses
+
+    def __hash__(self):
+        return hash((self.action, self.sub_clauses))
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.sub_clauses[0]!r})"
+
+    def __str__(self):
+        return f"{nested_str(self.sub_clauses[0])} {self.action}"
+
+
+class Action:
+    __slots__ = ('literal', '_py_source', '_py_code')
+    # TODO: Define these via a PEG parser
+    unpack = re.compile(r"\.\*")
+    named = re.compile(r"(^|[ (])\.([a-zA-Z]+)")
+    mangle = "__pika_act_"
+
+    def __init__(self, literal: str):
+        self.literal = literal
+        self._py_source = self._encode(literal)
+        self._py_code = compile(self._py_source, self._py_source, 'eval')
+
+    def __call__(self, __namespace, *args, **kwargs):
+        try:
+            return eval(self._py_code, __namespace)(*args, **kwargs)
+        except Exception as err:
+            raise type(err)(f"{err} <{self._py_source}>")
+
+    @classmethod
+    def _encode(cls, literal):
+        names = [f"{cls.mangle}{match.group(2)}" for match in cls.named.finditer(literal)]
+        body = cls.named.sub(
+            rf"\1 {cls.mangle}\2",
+            cls.unpack.sub(
+                rf" {cls.mangle}all", literal)
+        )
+        return f'lambda {cls.mangle}all, {", ".join(names)}: {body}'
+
+    def __str__(self):
+        return f"{{{self.literal}}}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.literal!r})"
+
+
+def transform(head: Match, memo: MemoTable, namespace: Dict[str, Any]):
+    return postorder_transform(head, memo.source, namespace)
+
+
+# TODO: Use trampoline/coroutines for infinite depth
+def postorder_transform(match: Match, source: D, namespace: Dict[str, Any]) -> Tuple[Any, Dict[str, Any]]:
+    matches, captures = (), {}
+    for sub_match in match.sub_matches:
+        sub_matches, sub_captures = postorder_transform(sub_match, source, namespace)
+        matches += sub_matches
+        captures.update(sub_captures)
+    position, clause = match.position, match.clause
+    if isinstance(clause, Capture):
+        assert len(matches) <= 1, "Captured rule must provide no more than one value"
+        captures[Action.mangle + clause.name] = matches[0] if matches else source[position:position + match.length]
+        return (), captures
+    elif isinstance(clause, Rule):
+        matches = matches if matches else source[position:position + match.length]
+        try:
+            result = clause.action(namespace, matches, **captures)
+        except Exception:
+            print(source[position:position + match.length])
+            raise
+        return (result,), {}
+    return matches, captures
---
+++ bootpeg/pika/boot.py
@@ -0,0 +1,143 @@
+import string
+import time
+import pathlib
+
+from .peg import Literal, Sequence, Choice, Nothing, Anything, Not, Repeat, Reference, Parser, postorder_dfs
+from .act import Debug, Capture, Rule, transform, Action
+from ..utility import ascii_escapes
+
+
+def range_parse(source: str, parser: Parser):
+    """Parse ``source`` showing the matched range"""
+    start = time.perf_counter()
+    match, memo = parser.parse(source)
+    until = time.perf_counter()
+    print(*(i % 10 for i in range(len(source))), sep='')
+    print(source.translate(ascii_escapes))
+    print('^', '-' * (match.length - 2), '^', sep='')
+    print(f"{until - start:.3f}s")
+    for define in transform(match, memo, namespace)[0]:
+        assert len(define) == 1, "Only implemented for one-define-per-match"
+        name, clause = next(iter(define.items()))
+        print(f"{name:<10} <-", clause)
+
+
+namespace = {
+    expression.__name__: expression for expression in
+    (Literal, Sequence, Choice, Nothing, Anything, Not, Repeat, Reference, Capture, Rule, Action)
+}
+
+
+Letters = Choice(*(Literal(ch) for ch in string.ascii_letters))
+spaces = Choice(Repeat(Literal(" ")), Nothing())
+end_line = Literal("\n")
+
+parser = Parser(
+    "top",
+    literal=Rule(
+        Choice(
+            *(
+                Sequence(quote, Repeat(Sequence(Not(quote), Anything())), quote)
+                for quote in (Literal("'"), Literal('"'))
+            )
+        ),
+        Action("Literal(.*)"),
+    ),
+    anything=Rule(
+        Literal("."),
+        Action("Literal(.*)"),
+    ),
+    identifier=Repeat(Choice(Letters, Literal("_"))),
+    choice=Rule(
+        Sequence(Capture("try", Reference("expr")), spaces, Literal("|"), spaces, Capture("else", Reference("expr"))),
+        Action("Choice(.try, .else)"),
+    ),
+    group=Rule(
+        Sequence(Literal("("), spaces, Capture("expr", Reference("expr")), spaces, Literal(")")),
+        Action(".expr"),
+    ),
+    sequence=Rule(
+        Sequence(Capture("head", Reference("expr")), spaces, Capture("tail", Reference("expr"))),
+        Action("Sequence(.head, .tail)"),
+    ),
+    repeat=Rule(
+        Sequence(Capture("base", Reference("expr")), spaces, Literal('+')),
+        Action("Repeat(.base)"),
+    ),
+    reference=Rule(
+        Reference("identifier"),
+        Action("Reference(.*)"),
+    ),
+    capture=Rule(
+        Sequence(Capture("name", Reference("identifier")), spaces, Literal("="), spaces, Capture("rule", Reference("expr"))),
+        Action("Capture(.name, .rule)"),
+    ),
+    reject=Rule(
+        Sequence(Literal("!"), Capture("expr", Reference("expr"))),
+        Action("Not(.expr)")
+    ),
+    expr=Rule(
+        Capture(
+            "expr",
+            Choice(
+                Reference("choice"), Reference("sequence"), Reference("repeat"), Reference("group"), Reference("capture"), Reference("reference"), Reference("literal"), Reference("reject"), Reference("anything")
+            )
+        ),
+        Action(".expr"),
+    ),
+    action=Rule(
+        Sequence(Literal("{"), Capture("body", Repeat(Sequence(Not(Literal("}")), Anything()))), Literal("}")),
+        Action("Action(.body)"),
+    ),
+    rule=Rule(
+        Sequence(Literal("|"), spaces, Capture("expr", Reference("expr")), spaces, Capture("action", Reference("action"))),
+        Action("Rule(.expr, .action)"),
+    ),
+    rules=Rule(
+        Capture(
+            "rule",
+            Choice(
+                Rule(
+                    Sequence(Literal(" "), spaces, Capture("try", Reference("rule")), spaces, end_line, Capture("else", Reference("rules"))),
+                    Action("Choice(.try, .else)"),
+                ),
+                Rule(
+                    Sequence(Literal(" "), spaces, Capture("rule", Reference("rule")), spaces, end_line),
+                    Action(".rule"),
+                ),
+            )
+        ),
+        Action(".rule"),
+    ),
+    define=Rule(
+        Sequence(Capture("name", Reference("identifier")), Literal(':'), spaces, end_line, Capture("rules", Reference("rules"))),
+        Action("{ .name : .rules }"),
+    ),
+    comment=Sequence(Literal("#"), Repeat(Sequence(Not(end_line), Anything())), end_line),
+    blank=Sequence(spaces, end_line),
+    top=Repeat(Choice(Reference("define"), Reference("comment"), Reference("blank"))),
+)
+print('--------------------------------------------')
+range_parse('''\
+#example
+hello:
+    | a "+" b | a {.*}
+    | c "+" d {.*}
+
+world:
+    | (a=a)+ {.*}
+''',
+parser
+)
+print('--------------------------------------------')
+with open(pathlib.Path(__file__).parent / 'boot.peg') as boot_peg:
+    range_parse(
+        boot_peg.read(),
+        parser,
+    )
+parser._prepare()
+print("Rules:", len(list(postorder_dfs(parser._compiled_parser[0][parser.top]))))
+for name, clause in parser._compiled_parser[0].items():
+    print(f"{name:<10} <-", clause)
+    if isinstance(clause, Debug):
+        print('Debugs:', clause.sub_clauses[0])
