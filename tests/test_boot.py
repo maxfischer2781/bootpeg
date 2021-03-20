fa753df2f62a82800d6ad0244536d318e86869a6
Max Fischer <maxfischer2781@gmail.com>
2021-03-20 17:03:39 +0100

    escalating self parsing

--- tests/test_boot.py
+++ tests/test_boot.py
@@ -2,8 +2,16 @@ from bootpeg.pika import boot
 
 
 def test_bootstrap():
-    with open(boot.boot_path) as boot_peg_stream:
-        boot_peg = boot_peg_stream.read()
+    boot_peg = boot.boot_path.read_text()
     parser = boot.parser
+    # ensure each parser handles itself
     for _ in range(2):
         parser = boot.boot(parser, boot_peg)
+
+
+def test_escalate():
+    boot_peg = boot.boot_path.read_text()
+    full_peg = boot.full_path.read_text()
+    parser = boot.boot(boot.parser, boot_peg)
+    for _ in range(2):
+        parser = boot.boot(parser, full_peg)
