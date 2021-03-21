c0090c9cb553788d0c85a5958c19fa7872f28e7d
Max Fischer <maxfischer2781@gmail.com>
2021-03-21 17:28:22 +0100

    black

--- bootpeg/utility.py
+++ bootpeg/utility.py
@@ -16,10 +16,12 @@ def mono(ascii: str) -> str:
 
 def cache_hash(__hash__):
     """Cache the ``__hash__`` of an object to a ``_hash`` attribute"""
+
     @wraps(__hash__)
     def cached_hash(self):
         _hash = self._hash
         if _hash is None:
             _hash = self._hash = __hash__(self)
         return _hash
+
     return cached_hash
