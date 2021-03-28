6d2b5492465aabbc31e505837c56f124f149cdd0
Max Fischer <maxfischer2781@gmail.com>
2021-03-28 18:19:54 +0200

    added parser factory/cache to boot

--- tests/test_boot.py
+++ tests/test_boot.py
@@ -5,7 +5,7 @@ from bootpeg.pika import boot
 
 def test_bootstrap():
     boot_peg = boot.boot_path.read_text()
-    parser = boot.parser
+    parser = boot.min_parser
     # ensure each parser handles itself
     for _ in range(2):
         parser = boot.boot(parser, boot_peg)
@@ -14,7 +14,7 @@ def test_bootstrap():
 def test_escalate():
     boot_peg = boot.boot_path.read_text()
     full_peg = boot.full_path.read_text()
-    parser = boot.boot(boot.parser, boot_peg)
+    parser = boot.boot(boot.min_parser, boot_peg)
     for _ in range(2):
         parser = boot.boot(parser, full_peg)
 
@@ -22,7 +22,7 @@ def test_escalate():
 def test_features():
     boot_peg = boot.boot_path.read_text()
     full_peg = boot.full_path.read_text()
-    parser = boot.boot(boot.boot(boot.parser, boot_peg), full_peg)
+    parser = boot.boot(boot.boot(boot.min_parser, boot_peg), full_peg)
     opt_repeat = boot.boot(parser, 'rule:\n    | [ " "+ ]\n')
     non_repeat = boot.boot(parser, 'rule:\n    | " "*\n')
     assert opt_repeat.clauses == non_repeat.clauses
