f9f57b375fd74e09395970405512c19e91ae8275
Max Fischer <maxfischer2781@gmail.com>
2021-05-28 19:31:30 +0200

    relying on pytest-cov for subprocess coverage

--- tests/test_examples/test_math.py
+++ tests/test_examples/test_math.py
@@ -1,11 +1,12 @@
 import subprocess
+import sys
 from bootpeg_examples import math
 
 
 def test_as_main():
     """Test the example as if run via `python -m`"""
     reply = subprocess.run(
-        ["coverage", "run", "-m", "bootpeg_examples.math"],
+        [sys.executable, "-m", "bootpeg_examples.math"],
         input="1\n2\nexit",
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
