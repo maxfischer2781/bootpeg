5a12d558125eafd896f3e3a624a0ef13d0b7cf6f
Max Fischer <maxfischer2781@gmail.com>
2021-05-28 19:21:03 +0200

    added math example and tests

---
+++ tests/test_examples/test_math.py
@@ -0,0 +1,14 @@
+import subprocess
+from bootpeg_examples import math
+
+
+def test_as_main():
+    """Test the example as if run via `python -m`"""
+    reply = subprocess.run(
+        ["coverage", "run", "-m", "bootpeg_examples.math"],
+        input="1\n2\nexit",
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    ).stdout.splitlines()
+    assert reply == [f"{math.prompt}{expected}" for expected in ("1", "2", "")]
