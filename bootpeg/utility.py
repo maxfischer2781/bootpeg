e852a20f92261211b352c6a6129034f5e71869dc
Max Fischer <maxfischer2781@gmail.com>
2021-07-06 19:51:23 +0200

    blackened

--- bootpeg/utility.py
+++ bootpeg/utility.py
@@ -73,7 +73,7 @@ def grammar_resource(location: str) -> Tuple[str, str]:
     return package, f"{name}.bpeg"
 
 
-T = TypeVar('T')
+T = TypeVar("T")
 
 
 def slotted(cls: Type[T]) -> Type[T]:
@@ -81,28 +81,25 @@ def slotted(cls: Type[T]) -> Type[T]:
     Class decorator to add ``__repr__``, ``__eq__`` and ``__hash__`` from ``__slots__``
     """
     # use dict comprehension to get unique insertion order
-    slots = tuple({
-        name: None
-        for scls in reversed(cls.__mro__)
-        for name in getattr(scls, "__slots__", ())
-    })
+    slots = tuple(
+        {
+            name: None
+            for scls in reversed(cls.__mro__)
+            for name in getattr(scls, "__slots__", ())
+        }
+    )
 
     def __repr__(self: T):
-        members = ", ".join(
-            f"{name}={getattr(self, name)}" for name in slots
-        )
+        members = ", ".join(f"{name}={getattr(self, name)!r}" for name in slots)
         return f"{self.__class__.__name__}({members})"
 
     def __eq__(self: T, other) -> bool:
         return isinstance(other, type(self)) and all(
-            getattr(self, name) == getattr(other, name)
-            for name in slots
+            getattr(self, name) == getattr(other, name) for name in slots
         )
 
     def __hash__(self: T) -> int:
-        return hash(
-            (*(getattr(self, name) for name in slots), hash(type(self)))
-        )
+        return hash((*(getattr(self, name) for name in slots), hash(type(self))))
 
     cls.__repr__ = __repr__
     cls.__eq__ = __eq__
