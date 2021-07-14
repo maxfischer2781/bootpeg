f32d33068c6c395b95c391f268b099454de28def
Max Fischer <maxfischer2781@gmail.com>
2021-07-14 18:25:52 +0200

    reworked bpeg definitions

--- bootpeg/grammars/bpeg.py
+++ bootpeg/grammars/bpeg.py
@@ -4,106 +4,121 @@ The default bootpeg grammar
 from typing import Union
 from functools import singledispatch
 
-from ..pika.peg import (
-    Clause,
-    Nothing,
-    Anything,
-    Literal,
+from ..apegs.boot import (
+    Value,
+    Range,
+    Any,
+    Empty,
     Sequence,
     Choice,
     Repeat,
-    Not,
     And,
+    Not,
+    Entail,
+    Capture,
+    Transform,
     Reference,
+    Rule,
+    Clause,
     Parser,
+    Grammar,
+    boot_parser,
 )
-from ..pika.act import Capture, Rule
-from ..pika.front import Range, Delimited
-from ..pika.boot import bootpeg, boot
 from ..api import bootpeg_actions, bootpeg_post, import_parser
 from ..typing import BootPegParser
 
 
+precedence = {
+    clause: prec for prec, clauses in enumerate(
+        (
+            [Value, Range, Any, Empty, Reference],
+            [Not, And, Capture],
+            [Repeat],
+            [Sequence, Entail],
+            [Choice, Transform],
+        )
+    ) for clause in clauses
+}
+
+
+def _wrapped(clause: Clause, parent: Clause) -> str:
+    return f"({unparse(clause)})" if precedence[parent] < precedence[Clause] else unparse(clause)
+
+
 @singledispatch
-def unparse(clause: Union[Clause, Parser], top=True) -> str:
+def unparse(clause: Union[Clause, Parser, Grammar]) -> str:
     """Format a ``clause`` according to bootpeg standard grammar"""
     raise NotImplementedError(f"Cannot unparse {clause!r} as bpeg")
 
 
-@unparse.register(Nothing)
-def unparse_nothing(clause: Nothing, top=True) -> str:
-    return '""'
-
-
-@unparse.register(Anything)
-def unparse_anything(clause: Anything, top=True) -> str:
-    return "." * clause.length
-
-
-@unparse.register(Literal)
-def unparse_literal(clause: Literal, top=True) -> str:
+@unparse.register(Value)
+def unparse_literal(clause: Value) -> str:
     return repr(clause.value)
 
 
-@unparse.register(Sequence)
-def unparse_sequence(clause: Sequence, top=True) -> str:
-    children = " ".join(
-        unparse(sub_clause, top=False) for sub_clause in clause.sub_clauses
-    )
-    return f"({children})" if not top else children
+@unparse.register(Range)
+def unparse_range(clause: Range) -> str:
+    return f"{clause.lower!r} - {clause.upper!r}"
 
 
-@unparse.register(Choice)
-def unparse_choice(clause: Choice, top=True) -> str:
-    children = " | ".join(
-        unparse(sub_clause, top=False) for sub_clause in clause.sub_clauses
-    )
-    return f"({children})" if not top else children
+@unparse.register(Empty)
+def unparse_empty(clause: Empty) -> str:
+    return '""'
 
 
-@unparse.register(Repeat)
-def unparse_repeat(clause: Repeat, top=True) -> str:
-    return unparse(clause.sub_clauses[0], top=False) + "+"
-
-
-@unparse.register(Not)
-def unparse_not(clause: Not, top=True) -> str:
-    return "!" + unparse(clause.sub_clauses[0], top=False)
-
-
-@unparse.register(And)
-def unparse_and(clause: And, top=True) -> str:
-    return "&" + unparse(clause.sub_clauses[0], top=False)
+@unparse.register(Any)
+def unparse_any(clause: Any) -> str:
+    return "." * clause.length
 
 
 @unparse.register(Reference)
-def unparse_reference(clause: Reference, top=True) -> str:
-    return clause.target
+def unparse_reference(clause: Reference) -> str:
+    return clause.name
+
+
+@unparse.register(Sequence)
+def unparse_sequence(clause: Sequence) -> str:
+    return " ".join(
+        _wrapped(sub_clause, clause) for sub_clause in clause.sub_clauses
+    )
+
+
+@unparse.register(Choice)
+def unparse_choice(clause: Choice) -> str:
+    return " | ".join(
+        _wrapped(sub_clause, clause) for sub_clause in clause.sub_clauses
+    )
+
+
+@unparse.register(Repeat)
+def unparse_repeat(clause: Repeat) -> str:
+    return _wrapped(clause.sub_clause, clause) + "+"
+
+
+@unparse.register(Not)
+def unparse_not(clause: Not) -> str:
+    return "!" + _wrapped(clause.sub_clause, clause)
+
+
+@unparse.register(And)
+def unparse_and(clause: And) -> str:
+    return "&" + _wrapped(clause.sub_clause, clause)
 
 
 @unparse.register(Capture)
-def unparse_capture(clause: Capture, top=True) -> str:
-    return f"{clause.name}={unparse(clause.sub_clauses[0], top=False)}"
+def unparse_capture(clause: Capture) -> str:
+    return f"{clause.name}={_wrapped(clause.sub_clause, clause)}"
 
 
 @unparse.register(Rule)
-def unparse_rule(clause: Rule, top=True) -> str:
-    return f"| {unparse(clause.sub_clauses[0])} {{{clause.action.literal}}}"
-
-
-@unparse.register(Range)
-def unparse_range(clause: Range, top=True) -> str:
-    return f"{clause.first!r} - {clause.last!r}"
-
-
-@unparse.register(Delimited)
-def unparse_delimited(clause: Delimited, top=True) -> str:
-    first, last = clause.sub_clauses
-    return f"{unparse(first, top=False)} :: {unparse(last, top=False)}"
+def unparse_rule(clause: Rule) -> str:
+    cases = clause.sub_clause.sub_clauses if isinstance(clause.sub_clause, Choice) else clause.sub_clause
+    body = "\n".join(f"    | {unparse(case)}" for case in cases)
+    return f"{clause.name}:\n{body}"
 
 
 def boot_dialect(source):
-    return boot(bootpeg(), source)
+    return boot_parser(source)[0]
 
 
 parse: BootPegParser[str, BootPegParser] = import_parser(
